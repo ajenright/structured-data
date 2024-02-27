d9bea19140eb108cd8ede9ca141d618304d2dcc2
Aaron J. Enright <153937848+ajenright@users.noreply.github.com>
2024-02-27 16:14:25 -0600

    I prefer sausage

--- pizza.py
+++ pizza.py
@@ -5,7 +5,7 @@ def print_pizza_order(pizza):
     print("With", pizza['toppings'], "on it")
 
 monday = {'size': "small", 'crust': "traditional", 'toppings': "extra cheese"}
-friday = {'size': "large", 'crust': "deep dish", 'toppings': "pepperoni"}
+friday = {'size': "large", 'crust': "deep dish", 'toppings': "sausage"}
 
 print_pizza_order(monday)
 
